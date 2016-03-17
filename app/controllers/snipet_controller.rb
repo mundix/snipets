1b121b329bc4afd246062b80c8069cde3330a361
Edmundo Pichardo <clemente.pichardo@theoreminc.net>
2016-03-17 17:55:04 -0400

    Se generaron los archivos html con relacion al controller snipets

--- app/controllers/snipet_controller.rb
+++ app/controllers/snipet_controller.rb
@@ -1,6 +1,7 @@
 class SnipetController < ApplicationController
+  before_action  :set_snipet, only: [:edit,:update,:show,:destroy]
   def index
-
+    @snipets = Snipet.all
   end
 
   def show
@@ -24,8 +25,12 @@ class SnipetController < ApplicationController
   end
 
   private
-    def set_param
+    def set_snipet
+      @snipet = Snipet.find_by(params[:id])
+    end
 
+    def snipet_param
+      params.require(:snipet).permit(:title, :body)
     end
 
 end
